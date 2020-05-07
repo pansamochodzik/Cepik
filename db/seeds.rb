a4cf2dd2abc807857032f0822dbdd3606822a8a4
pansamochodzik <tomaszadach77@gmail.com>
2020-05-07 14:33:00 +0200

    Seed fake data to database

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,28 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
+Car.destroy_all
+
+# Car.create!([
+#   {name:"bmw",color: "red"},
+#   {name:"mercedes",color: "purple"},
+#   {name:"opel",color: "yellow"},
+#   {name:"fiat",color: "white"},
+#   {name:"mazda",color: "blue"},
+#   ])
 #
-# Examples:
+# p "Created #{Car.count} cars"
+
+
+# 30.times do |index|
+#   Car.create!(name: "name_#{index}",color: "color_#{index}")
+# end
 #
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+# p "Created #{Car.count} cars"
+
+
+30.times do
+  Car.create!(
+    name: Faker::Vehicle.manufacture ,
+    color: Faker::Vehicle.color
+  )
+end
+
+p "Created #{Car.count} cars"
