efd23c35d63a332b4c153796283ff253af8b5958
pansamochodzik <tomaszadach77@gmail.com>
2020-05-08 16:18:55 +0200

    Building forms to add cars to list

--- app/controllers/cars_controller.rb
+++ app/controllers/cars_controller.rb
@@ -1,5 +1,4 @@
 class CarsController < ApplicationController
-
   def index
     @cars = Car.all
   end
@@ -8,4 +7,22 @@ class CarsController < ApplicationController
     @car = Car.find(params[:id])
   end
 
+  def new
+    @car = Car.new
+  end
+
+  def create
+    @car = Car.new(new_car_params)
+    if @car.save
+      redirect_to cars_path
+    else
+      render 'new'
+    end
+  end
+
+  private
+
+  def new_car_params
+    params.require(:car).permit(:name, :colour)
+  end
 end
