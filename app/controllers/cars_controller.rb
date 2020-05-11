9e9e248b4d39b9dfe3d89c7aade74226b69319c5
pansamochodzik <tomaszadach77@gmail.com>
2020-05-11 14:16:49 +0200

    Repair rendering of the form

--- app/controllers/cars_controller.rb
+++ app/controllers/cars_controller.rb
@@ -16,7 +16,7 @@ class CarsController < ApplicationController
     if @car.save
       redirect_to cars_path
     else
-      render 'new'
+      render :new
     end
   end
 
@@ -35,7 +35,7 @@ class CarsController < ApplicationController
     if @car.update(car_params)
       redirect_to cars_path
     else
-      render 'new'
+      render :new
     end
   end
 
