c224f04a3b856f89d5b9bb39dc50bf898a4f0242
pansamochodzik <tomaszadach77@gmail.com>
2020-05-11 20:37:27 +0200

    Change car list table and form considering new data and added new validation

--- app/models/car.rb
+++ app/models/car.rb
@@ -1,3 +1,3 @@
 class Car < ApplicationRecord
-  validates :name, :colour, presence: true
+  validates :name, :colour, :vin_number, :license_plate, :production_at, :registration_at, presence: true
 end
