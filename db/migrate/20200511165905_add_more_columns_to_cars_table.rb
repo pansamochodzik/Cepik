eb99019eea98b3d2c7aa02a008c3dbadc9524988
pansamochodzik <tomaszadach77@gmail.com>
2020-05-11 20:35:00 +0200

    Create new columns in cars table.

---
+++ db/migrate/20200511165905_add_more_columns_to_cars_table.rb
@@ -0,0 +1,8 @@
+class AddMoreColumnsToCarsTable < ActiveRecord::Migration[5.2]
+  def change
+    add_column :cars, :vin_number, :string
+    add_column :cars, :license_plate, :string
+    add_column :cars, :production_at, :string
+    add_column :cars, :registration_at, :string
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_05_07_131426) do
+ActiveRecord::Schema.define(version: 2020_05_11_165905) do
 
   create_table "cars", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "colour"
+    t.string "vin_number"
+    t.string "license_plate"
+    t.string "production_at"
+    t.string "registration_at"
   end
 
 end
