d6db965218e1ac34ff50d774d99a3e55f1b3e3d5
pansamochodzik <tomaszadach77@gmail.com>
2020-05-11 20:36:13 +0200

    Clear old data and created new considering new column

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,7 +6,11 @@ p "Creating Cars"
 30.times do
   Car.create!(
     name: Faker::Vehicle.manufacture,
-    colour: Faker::Vehicle.color
+    colour: Faker::Vehicle.color,
+    vin_number: Faker::Vehicle.vin,
+    license_plate: Faker::Vehicle.license_plate,
+    production_at: @production = Faker::Vehicle.year,
+    registration_at: @production + Random.rand(0..2)
   )
 end
 
