c682417915d29237e658ba538ae3e26eccef1456
pansamochodzik <tomaszadach77@gmail.com>
2020-05-13 19:45:00 +0200

    deleted command hidding warning

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -1,5 +1,5 @@
 # Be sure to restart your server when you modify this file.
-Warning[:deprecated] = false
+
 # ActiveSupport::Reloader.to_prepare do
 #   ApplicationController.renderer.defaults.merge!(
 #     http_host: 'example.org',
