8ded652a5862a3b81ac14b9b51a23e866814cfcc
pansamochodzik <tomaszadach77@gmail.com>
2020-05-15 17:56:17 +0200

    changes in seeds.rb and subjecting them to validation

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,6 @@
+require 'database_cleaner'
+DatabaseCleaner.clean_with(:truncation)
+
 p "Creating Cars"
 
 30.times do
@@ -6,8 +9,15 @@ p "Creating Cars"
     colour: Faker::Vehicle.color,
     vin_number: Faker::Vehicle.vin,
     license_plate: Faker::Vehicle.license_plate,
-    year_of_production: @production = Faker::Vehicle.year,
-    year_of_registration: @production + Random.rand(0..2)
+    year_of_production: @year_of_production = Faker::Vehicle.year,
+    year_of_registration: 
+      if @year_of_production == Time.now.year
+        @year_of_production
+      elsif @year_of_production <= Time.now.year && @year_of_production >= 2019
+        @year_of_production + Random.rand(0..1)
+      else
+        @year_of_production + Random.rand(0..2)
+      end
   )
 end
 
