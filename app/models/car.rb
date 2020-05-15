b9aaff5b8a6d47057f9c80cb4b56a888f4f27f0c
pansamochodzik <tomaszadach77@gmail.com>
2020-05-15 17:53:47 +0200

    Specifing validation

--- app/models/car.rb
+++ app/models/car.rb
@@ -1,4 +1,8 @@
 class Car < ApplicationRecord
-  validates :name, :colour, :vin_number, :license_plate, :year_of_production, :year_of_registration, presence: true
-  validates :vin_number, :license_plate, uniqueness: true
+  validates :name, presence: true, format: { with: /\A[a-zA-Z\-\s]+\z/, message: " can only inlude letters,hyphen and spaces"}
+  validates :colour, presence: true, format: { with: /\A[a-zA-Z]+\z/, message: " can only inlude letters"}
+  validates :vin_number, presence: true, uniqueness: true, format: { with: /\A[a-zA-Z0-9]+\z/, message: " can only inlude letters and numbers"}
+  validates :license_plate, presence: true, uniqueness: true, format: { with: /\A[a-zA-Z0-9\-]+\z/, message: " can only inlude letters, numbers and hyphen"}
+  validates :license_plate, presence: true, uniqueness: true, format: { with: /\A[a-zA-Z0-9\-]+\z/, message: " can only inlude letters, numbers and hyphen"}
+  validates :year_of_production, :year_of_registration, presence: true, :length => { is: 4, message: " should be 4 digit and only digit" }, numericality: true, inclusion: { in: 1900..Time.now.year, message: " should be between 1900 and the current year"}
 end
