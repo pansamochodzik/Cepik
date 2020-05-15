a8a7f92ff2e1efdf0ffee3a14a91b871cb4db931
pansamochodzik <tomaszadach77@gmail.com>
2020-05-15 19:11:49 +0200

    Nothing changes but i can't discard this nothing.

--- db/seeds.rb
+++ db/seeds.rb
@@ -10,7 +10,7 @@ p "Creating Cars"
     vin_number: Faker::Vehicle.vin,
     license_plate: Faker::Vehicle.license_plate,
     year_of_production: @year_of_production = Faker::Vehicle.year,
-    year_of_registration: 
+    year_of_registration:
       if @year_of_production == Time.now.year
         @year_of_production
       elsif @year_of_production <= Time.now.year && @year_of_production >= 2019
