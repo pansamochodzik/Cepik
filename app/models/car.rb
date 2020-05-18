c211996d7c56afb8f1a14f05f5d38d4954263b7c
pansamochodzik <tomaszadach77@gmail.com>
2020-05-18 17:22:22 +0200

    changing and formating validation

--- app/models/car.rb
+++ app/models/car.rb
@@ -1,8 +1,17 @@
 class Car < ApplicationRecord
-  validates :name, presence: true, format: { with: /\A[a-zA-Z\-\s]+\z/, message: " can only inlude letters,hyphen and spaces"}
-  validates :colour, presence: true, format: { with: /\A[a-zA-Z]+\z/, message: " can only inlude letters"}
-  validates :vin_number, presence: true, uniqueness: true, format: { with: /\A[a-zA-Z0-9]+\z/, message: " can only inlude letters and numbers"}
-  validates :license_plate, presence: true, uniqueness: true, format: { with: /\A[a-zA-Z0-9\-]+\z/, message: " can only inlude letters, numbers and hyphen"}
-  validates :license_plate, presence: true, uniqueness: true, format: { with: /\A[a-zA-Z0-9\-]+\z/, message: " can only inlude letters, numbers and hyphen"}
-  validates :year_of_production, :year_of_registration, presence: true, :length => { is: 4, message: " should be 4 digit and only digit" }, numericality: true, inclusion: { in: 1900..Time.now.year, message: " should be between 1900 and the current year"}
+  validates :name, presence: true
+  validates :name, format: { with: /\A[a-zA-Z]+\z/, message: ' can only inlude letters'}
+  validates :name, :length => { maximum: 15, message: ' should be short one and in one word, maximum 15 characters'}
+  validates :colour, presence: true
+  validates :colour,  format: { with: /\A[a-zA-Z]+\z/, message: ' can only inlude letters'}
+  validates :vin_number, presence: true, uniqueness: true
+  validates :vin_number, format: { with: /\A[a-zA-Z0-9]+\z/, message: ' can only inlude letters and numbers'}
+  validates :license_plate, presence: true, uniqueness: true
+  validates :license_plate, format: { with: /\A[a-zA-Z0-9\-]+\z/, message: ' can only inlude letters, numbers and hyphen'}
+  validates :year_of_production, presence: true, numericality: true
+  validates :year_of_production, :length => { is: 4, message: ' should be 4 digit and only digit' }
+  validates :year_of_production, inclusion: { in: 1900..Time.now.year, message: ' should be between 1900 and the current year'}
+  validates :year_of_registration, presence: true, , numericality: true
+  validates :year_of_registration, :length => { is: 4, message: ' should be 4 digit and only digit' }
+  validates :year_of_registration, inclusion: { in: 1900..Time.now.year, message: ' should be between 1900 and the current year'}
 end
