dd9aa34b6aa57399e1fb4f2720d213569cc3717f
pansamochodzik <tomaszadach77@gmail.com>
2020-05-18 23:14:38 +0200

    more changing and formating validation

--- app/models/car.rb
+++ app/models/car.rb
@@ -1,17 +1,12 @@
 class Car < ApplicationRecord
-  validates :name, presence: true
-  validates :name, format: { with: /\A[a-zA-Z]+\z/, message: ' can only inlude letters'}
-  validates :name, :length => { maximum: 15, message: ' should be short one and in one word, maximum 15 characters'}
-  validates :colour, presence: true
-  validates :colour,  format: { with: /\A[a-zA-Z]+\z/, message: ' can only inlude letters'}
-  validates :vin_number, presence: true, uniqueness: true
-  validates :vin_number, format: { with: /\A[a-zA-Z0-9]+\z/, message: ' can only inlude letters and numbers'}
-  validates :license_plate, presence: true, uniqueness: true
-  validates :license_plate, format: { with: /\A[a-zA-Z0-9\-]+\z/, message: ' can only inlude letters, numbers and hyphen'}
-  validates :year_of_production, presence: true, numericality: true
-  validates :year_of_production, :length => { is: 4, message: ' should be 4 digit and only digit' }
-  validates :year_of_production, inclusion: { in: 1900..Time.now.year, message: ' should be between 1900 and the current year'}
-  validates :year_of_registration, presence: true, , numericality: true
-  validates :year_of_registration, :length => { is: 4, message: ' should be 4 digit and only digit' }
-  validates :year_of_registration, inclusion: { in: 1900..Time.now.year, message: ' should be between 1900 and the current year'}
+  validates :name, format: {with: /\A[a-zA-Z]{1,10}\z/, message: ' can only inlude letters and have maximum 10 characters. Cannot be blank'}
+  validates :colour, format: {with: /\A[a-zA-Z]{1,15}\z/, message: ' can only inlude letters and have maximum 15 characters. Cannot be blank.'}
+  validates :vin_number, uniqueness: true, :length => {is: 17}
+  validates :vin_number, format: {with: /\A[A-Z0-9]+\z/, message: ' can only inlude capital letters and numbers/'}
+  validates :license_plate, format: {with: /\A[A-Z0-9]{2,3}[\d-][A-Z0-9]{4,5}\z/, message: ' can only inlude capital letters, numbers and hyphen not space. Cannot be blank.'}
+  validates :license_plate, uniqueness: true, :length => {is: 8}
+  validates :year_of_production, numericality: true, :length => {is: 4}
+  validates :year_of_production, inclusion: {in: 1900..Time.now.year, message: ' should be between 1900 and the current year.'}
+  validates :year_of_registration, numericality: true, :length => {is: 4}
+  validates :year_of_registration, inclusion: {in: 1900..Time.now.year, message: ' should be between 1900 and the current year.'}
 end
