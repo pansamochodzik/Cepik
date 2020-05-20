4b1bd50f572d11cb24aa0bf754727410b521d83b
pansamochodzik <tomaszadach77@gmail.com>
2020-05-20 14:59:44 +0200

    improvement typos and formatting

--- app/models/car.rb
+++ app/models/car.rb
@@ -4,7 +4,7 @@ class Car < ApplicationRecord
   validates :colour, format: { with: /\A[a-zA-Z\d-]{1,20}\z/ }
 
   validates :vin_number, uniqueness: true,
-                         length: {is: 17},
+                         length: { is: 17 },
                          format: { with: /\A[A-Z0-9]+\z/ }
 
   validates :license_plate, format: { with: /\A[A-Z0-9]{2,3}[\d-][A-Z0-9]{4,5}\z/ },
