8042429657e147b2d55a483b235059d5e4682b38
pansamochodzik <tomaszadach77@gmail.com>
2020-05-29 14:45:56 +0200

    Formatting changes suggested by Rubocop

--- db/schema.rb
+++ db/schema.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 # This file is auto-generated from the current state of the database. Instead
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
@@ -10,22 +12,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_05_13_140917) do
-
+ActiveRecord::Schema.define(version: 20_200_513_140_917) do
   # These are extensions that must be enabled in order to support this database
-  enable_extension "plpgsql"
+  enable_extension 'plpgsql'
 
-  create_table "cars", force: :cascade do |t|
-    t.string "name"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.string "colour"
-    t.string "vin_number"
-    t.string "license_plate"
-    t.integer "year_of_production"
-    t.integer "year_of_registration"
-    t.index ["license_plate"], name: "index_cars_on_license_plate", unique: true
-    t.index ["vin_number"], name: "index_cars_on_vin_number", unique: true
+  create_table 'cars', force: :cascade do |t|
+    t.string 'name'
+    t.datetime 'created_at', null: false
+    t.datetime 'updated_at', null: false
+    t.string 'colour'
+    t.string 'vin_number'
+    t.string 'license_plate'
+    t.integer 'year_of_production'
+    t.integer 'year_of_registration'
+    t.index ['license_plate'], name: 'index_cars_on_license_plate', unique: true
+    t.index ['vin_number'], name: 'index_cars_on_vin_number', unique: true
   end
-
 end
