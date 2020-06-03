7630218f4f71a9f5686682e218c489a102a0dfed
pansamochodzik <tomaszadach77@gmail.com>
2020-06-03 22:09:34 +0200

    schema conflict

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_05_13_140917) do
+ActiveRecord::Schema.define(version: 2020_06_03_123735) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
