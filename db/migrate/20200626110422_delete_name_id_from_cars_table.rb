e38ffe19a806da78d1bd6ccbb51be5bfe8c3ce34
pansamochodzik <tomaszadach77@gmail.com>
2020-06-26 18:19:58 +0200

    wip

--- db/migrate/20200626110422_delete_name_id_from_cars_table.rb
+++
@@ -1,5 +0,0 @@
-class DeleteNameIdFromCarsTable < ActiveRecord::Migration[5.2]
-  def change
-    remove_column :cars, :name_id
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -24,19 +24,9 @@ ActiveRecord::Schema.define(version: 2020_06_26_111722) do
     t.string "license_plate"
     t.integer "year_of_production"
     t.integer "year_of_registration"
-    t.string "registration_country"
     t.string "name"
     t.index ["license_plate"], name: "index_cars_on_license_plate", unique: true
     t.index ["vin_number"], name: "index_cars_on_vin_number", unique: true
-  end
-
-  create_table "countries", force: :cascade do |t|
-    t.string "country"
-    t.string "country_code"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["country"], name: "index_countries_on_country", unique: true
-    t.index ["country_code"], name: "index_countries_on_country_code", unique: true
   end
 
 end
