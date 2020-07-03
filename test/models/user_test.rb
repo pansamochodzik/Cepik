c973fd6fae16eb2ae9f9be6bbff68751609e4a91
pansamochodzik <tomaszadach77@gmail.com>
2020-07-03 13:50:06 +0200

    Tests for model user

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,7 +1,29 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  def setup
+    @user = users(:user)
+  end
+
+  test 'user will be created correctly?' do
+    assert @user.valid?
+  end
+
+  test 'user password can have lass than 8 characters?' do
+    @user.password = 'passwor'
+
+    assert @user.invalid?
+  end
+
+  test 'user email can be without sign @?' do
+    @user.email = 'test.com'
+
+    assert @user.invalid?
+  end
+
+  test 'user email can have two or more sign @?' do
+    @user.email = 'test@test@test.com'
+
+    assert @user.invalid?
+  end
 end
