a6aeac7a366a176cb8990f362f976c411bf16e2a
pansamochodzik <tomaszadach77@gmail.com>
2020-07-03 14:02:55 +0200

    Config mailer to production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -2,7 +2,18 @@
 
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
+  config.action_mailer.default_url_options = { :host => 'cepik-v1.herokuapp.com' }
 
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    address: "smtp.gmail.com",
+    domain: "gmail.com",
+    port: 587,
+    user_name: "wekrakersed",
+    password: "mondeo77",
+    authentication: 'plain',
+    enable_starttls_auto: true
+  }
   # Code is not reloaded between requests.
   config.cache_classes = true
 
