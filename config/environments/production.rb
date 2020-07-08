e270562a1e44f3ee66d9be5d906d2353408b0a89
pansamochodzik <tomaszadach77@gmail.com>
2020-07-08 16:57:28 +0200

    Add sendgrid. Secure important data.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -6,11 +6,11 @@ Rails.application.configure do
 
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
-    address: "smtp.gmail.com",
-    domain: "gmail.com",
+    address: 'smtp.sendgrid.net',
+    domain: 'gmail.com',
     port: 587,
-    user_name: "wekrakersed",
-    password: "mondeo77",
+    user_name: ENV['SENDGRID_USER'],
+    password: ENV['SENDGRID_PASSWORD'],
     authentication: 'plain',
     enable_starttls_auto: true
   }
