531ef5367a9d32c9800fcc9e66fe630cbbbf5359
pansamochodzik <tomaszadach77@gmail.com>
2020-07-30 16:11:42 +0200

    wip

--- test/system/user_test.rb
+++ test/system/user_test.rb
@@ -22,9 +22,26 @@ class CarTest < ApplicationSystemTestCase
     click_on 'Log out'
 
     visit new_user_session_url
-    fill_in 'Email', text: 'test@test.pl'
+    fill_in 'Email', with: 'test@test.pl'
     click_on 'Log in'
 
-    assert_selector
+    assert_selector 'h2', text: 'Invalid Email or password.'
+  end
+
+  test 'invalid password 10 times and lock user(failed attempts)' do
+    visit root_url
+    click_on 'Log out'
+
+    10.times do
+      visit new_user_session_url
+
+      fill_in 'Email', with: @user.email
+      fill_in 'Password', with: 'passwor'
+
+      click_on 'Log in'
+    end
+
+    assert_selector 'h2', text: 'Your account is locked.'
   end
 end
+# You have one more attempt before your account is locked.
